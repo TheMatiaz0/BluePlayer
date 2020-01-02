1d409e35ed92e96087c14aac0efbe207fe3a38e7
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-01-02 11:51:44 +0100

    Initial commit

---
+++ MusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<MusicTrack> musicTracks = new ObservableCollection<MusicTrack>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            musicTracks = new ObservableCollection<MusicTrack>() { new MusicTrack("Arctic Monkeys", "I Wanna Be Yours"), new MusicTrack("Radiohead", "Creep") };
+
+            musicTracks.Add(new MusicTrack("Test", "Just a Test"));
+
+            AllMusicTracks.ItemsSource = musicTracks;
+
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            musicTracks.Add(new MusicTrack("Trine", "Eat my wine"));
+        }
+    }
+}
---
+++ MusicPlayer/MusicTrack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class MusicTrack
+    {
+        public MusicTrack(string artist, string songName)
+        {
+            Artist = artist;
+            SongName = songName;
+        }
+
+        public string Artist { get; }
+        public string SongName { get; set; }
+    }
+}
